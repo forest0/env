283df4d9982dfc26653128c3dd57becf2cd54d45
forest0 <forest9643@gmail.com>
2020-12-31 16:24:12 +0800

    Set environment variable according to os type

--- shell/envs.sh
+++ shell/envs.sh
@@ -5,23 +5,25 @@ export GO111MODULE=on
 export GOPATH=~/workspace/go
 export PATH=$PATH:$GOPATH/bin
 
-# tex
-export PATH=$PATH:/usr/local/texlive/2019basic/bin/x86_64-darwin
-
 export EDITOR=nvim
 
-export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # tex
+    export PATH=$PATH:/usr/local/texlive/2020basic/bin/x86_64-darwin
 
-# search /usr/local/bin first
-export PATH="/usr/local/bin:$PATH"
+    # search /usr/local/bin first
+    export PATH="/usr/local/bin:$PATH"
 
-# use gnu bin
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+    # use gnu bin
+    export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 
-# It is ok to use global proxy, since mellow will route traffic properly.
-# Although mellow use a tun by default(which will take over all traffic
-# transparently), it has a performance problem.
-#
-# TODO: check port available
-export http_proxy=http://127.0.0.1:2885/
-export https_proxy=http://127.0.0.1:2885/
+    # It is ok to use global proxy, since mellow will route traffic properly.
+    # Although mellow use a tun by default(which will take over all traffic
+    # transparently), it has a performance problem.
+    #
+    # TODO: check port available
+    export http_proxy=http://127.0.0.1:2885/
+    export https_proxy=http://127.0.0.1:2885/
+fi
+
+
