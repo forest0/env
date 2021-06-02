a075cfdacc49c9c85256c0d4bafa749bfc0c0bdd
forest0 <forest9643@gmail.com>
2021-06-02 19:27:24 +0800

    Disable treesitter highlight for bash

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,8 @@ require'nvim-treesitter.configs'.setup {
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true,              -- false will disable the whole extension
-    disable = {},  -- list of language that will be disabled
+    disable = {  -- list of language that will be disabled
+      "bash",     -- bash's highlight by treesitter is not that good at present(2021-06-02)
+    },
   },
 }
